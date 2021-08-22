4d4da2440016f3616b4300e7aff53337a1eb244d
Kujima <bertaux.martin@gmail.com>
2021-08-22 18:05:36 +0200

    méthodes pour récupérer le nom et class d'un personnage opérationnelles

--- Bdo.Characters.info.Data/Repositories/CharactersRepositorie.cs
+++ Bdo.Characters.info.Data/Repositories/CharactersRepositorie.cs
@@ -1,4 +1,5 @@
 ﻿using Bdo.Characters.info.Data.DataProviders;
+using Bdo.Characters.info.Data.Models;
 using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
@@ -21,14 +22,69 @@ namespace Bdo.Characters.info.Data.Repositories
             
             
 
-        public async Task<object> GetCharacters(string characterName)
+        public async Task<Family> GetCharacters(string characterName)
         {
             string htmlPageSearchFamily = await _charactersProvider.GetPageSearchFamily(characterName);
             string urlFamilyInfo = GetUrlFamilyInfo(htmlPageSearchFamily);
             string htmlPageFamilyInfo = await _charactersProvider.GetPageFamilyInfo(urlFamilyInfo);
+            string familyName = ScrappFamilyName(htmlPageFamilyInfo);
             var charactersInfoString = ScrappHtmlCharacterInfo(htmlPageFamilyInfo);
 
-            throw new NotImplementedException();
+
+            Family family = new()
+            {
+                Name = familyName,
+                Characters = SetCharactersObject(charactersInfoString)
+            };
+
+            return family;
+        }
+
+
+        private List<Character> SetCharactersObject(HtmlNodeCollection charactersInfoString)
+        {
+            List<Character> characters = new();
+
+            foreach (HtmlNode htmlNodeCharacter in charactersInfoString)
+            {
+                _htmlDoc.LoadHtml(htmlNodeCharacter.InnerHtml.ToString());
+
+                characters.Add(new Character()
+                {
+                    Name = CleanNameString(_htmlDoc.DocumentNode.SelectSingleNode("//p[@class='character_name']").InnerText),
+                    Class = _htmlDoc.DocumentNode.SelectSingleNode("/div[1]/div[1]/p[2]/span/em[2]").InnerText,
+                    Professions = SetProfessions()
+                }); 
+
+            }
+
+            return characters;
+        }
+
+        /// <summary>
+        /// enlève les résidu html dans la variable
+        /// </summary>
+        /// <param name="nameToClean"></param>
+        /// <returns></returns>
+        private string CleanNameString(string nameToClean)
+        {
+            string nameClean = nameToClean.Replace(@"\r\n", "").Trim();
+
+            return nameClean;
+        }
+
+
+        /// <summary>
+        /// Scrapp le nom de la famille
+        /// </summary>
+        /// <param name="htmlPageFamilyInfo"></param>
+        /// <returns></returns>
+        private string ScrappFamilyName(string html)
+        {
+            _htmlDoc.LoadHtml(html);
+            string familyName = _htmlDoc.DocumentNode.SelectSingleNode("//p[@class='nick']").InnerText;
+            
+            return familyName;
         }
 
         /// <summary>
@@ -42,10 +98,15 @@ namespace Bdo.Characters.info.Data.Repositories
             var htmlNodes = _htmlDoc.DocumentNode.SelectNodes("//a[@href]");
             var linkFound = htmlNodes.Where(a => a.GetAttributeValue("href", string.Empty).Contains("https://www.naeu.playblackdesert.com/Adventure/Profile")).First();
             string link = linkFound.GetAttributeValue("href", string.Empty);
-
+            
             return link;
         }
 
+        /// <summary>
+        /// Récupère la liste d'information de tous les caractère de la famille 
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
         private HtmlNodeCollection ScrappHtmlCharacterInfo(string html)
         {
             _htmlDoc.LoadHtml(html);
@@ -53,5 +114,7 @@ namespace Bdo.Characters.info.Data.Repositories
 
             return charactersInfo;
         }
+
+
     }
 }
